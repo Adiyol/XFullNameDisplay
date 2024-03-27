18053a7b66173c85f3c4f2663b17709fe11a3ff8
lolers <adithyajayan@gmail.com>
2024-03-27 15:49:22 +0530

    full name does not display initially if an one of the fields does not pass validation

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,38 @@ function App() {
   const [fullName, setFullName] = useState("");
   function onSubmit(e) {
     e.preventDefault()
+    console.log(firstName.current.value, lastName.current.value, "values")
     setFullName(firstName.current.value + " " + lastName.current.value);
+  }
+
+  // function onInvalid(e) {
+  //   console.log("Invalid")
+  // }
+
+  function checkFilled() {
+    if(firstName.current && lastName.current) {
+      if(firstName.current.value && lastName.current.value) {
+        return true
+      } else {
+         false
+      }
+    } else {
+      return false
+    }
   }
   return (
     <>
       <h1>Full Name Display</h1>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} >
         <label>First Name: </label>
         <input type="text" ref={firstName} required></input>
         <br />
         <label>Last Name: </label>
-        <input type="text" ref={lastName} required></input>
+        <input type="text" ref={lastName} required ></input>
         <br />
-        <button type="submit">Submit</button>
+        <button  type="submit">Submit</button>
       </form>
-        <p>{"Full Name: " + fullName}</p>
+        {(checkFilled()) ? (<p>{"Full Name: " + fullName}</p>) : null}
       
     </>
   );
