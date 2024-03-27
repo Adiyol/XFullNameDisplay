dbeefe30193501b17270ed9b2b0fbdea87f566c6
lolers <adithyajayan@gmail.com>
2024-03-27 14:57:28 +0530

    XFullNameDisplay

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  let firstName = useRef();
+  let lastName = useRef();
+
+  const [fullName, setFullName] = useState("");
+  function onSubmit(e) {
+    e.preventDefault()
+    setFullName(firstName.current.value + " " + lastName.current.value);
+  }
+  return (
+    <>
+      <h1>Full Name Display</h1>
+      <form onSubmit={onSubmit}>
+        <label>First Name: </label>
+        <input type="text" ref={firstName} required></input>
+        <br />
+        <label>Last Name: </label>
+        <input type="text" ref={lastName} required></input>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {fullName}
+    </>
+  );
+}
+
+export default App;
